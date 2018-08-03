ff5e01d79be1f91ed9fda9e4904136578714aa8a
Zoey Ikemoto <zmi384@gmail.com>
2018-08-02 20:04:54 -0700

    all get() tests pass

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 
 const app = express();
-const { details } = require('../data/mockpagedetail.js');
+const inventory = require('../data/mockpagedetail');
 
 app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/rooms/', (req, res) => {
+//   console.log(inventory);
+//   res.send(inventory);
+// });
 app.get('/rooms/:roomId', (req, res) => {
-//  result = req.params;
-  res.send("oh! that's a room id");
+  let result = inventory.find(function(listing) {
+    return listing.id + '' === req.params.roomId;
+  });
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404)
+      .send('Listing not found.');
+  }
 });
 
-const server = app.listen(3004, () => console.log('Example app listening on port 3004!'));
+const server = app.listen(3004, () => console.log('Inventory service listening on port 3004!'));
 
 module.exports = server;
